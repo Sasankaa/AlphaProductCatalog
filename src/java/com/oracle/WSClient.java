d8207bcc27da7e5f332562275d5f917640fd1598
Sasanka Abeysinghe <sasanka.abeysinghe@ttu.edu>
2018-07-12 11:45:58 -0500

    Json string mapping

--- src/java/com/oracle/WSClient.java
+++ src/java/com/oracle/WSClient.java
@@ -8,6 +8,18 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  *
@@ -18,12 +30,37 @@ public class WSClient {
         Client client = ClientBuilder.newClient();
         String output="Default";
         WebTarget target = client.target("http://localhost:8080/statictweets");
-        //using response object to get more information
+        
         Response s = target.request().get();
-        //System.out.println("response: " + s.getEntity() + ",  status " + s.getStatus());
-        //output = "response: " + s.getEntity() + ",  status " + s.getStatus();
         output = target.request().get(String.class);
+        //output = output.replace("{", "\"{ \\\"");
+        ObjectMapper objectMapper = new ObjectMapper();
+        //String tweetsJson = "{ \"id\" : \"765626469619859500\"}";
+        output = output.replace("id", "\"id\"");
+        String tweetsJson = output;
+        String filterOutput = "";
+        try {
+          tweets twt = objectMapper.readValue(tweetsJson, tweets.class);
+            filterOutput += twt.getId();
+        } catch (IOException ex) {
+            Logger.getLogger(WSClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+
+		
+        
+
+        //String[] lines = output.split(",");
+        //for (String line : lines) {
+        //    output += line +"\\n";
+        //}
         s.close();
-        return output;
+        return filterOutput;
+    }
+    private static void requestOrderById(Client client, String id) {
+        WebTarget target = client.target("http://localhost:8080/statictweets" +id);
+        String s = target.request().get(String.class);
+        System.out.println("response : " + s);
     }
 }
