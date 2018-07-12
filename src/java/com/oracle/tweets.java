8111afba616e57a236513ca54eecef419d8d535f
Sasanka Abeysinghe <sasanka.abeysinghe@ttu.edu>
2018-07-12 11:48:51 -0500

    Json string mapping

---
+++ src/java/com/oracle/tweets.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oracle;
+
+/**
+ *
+ * @author sasanka
+ */
+public class tweets {
+    private String id = null;
+    private String user = null;
+    
+    public String getId(){
+        return this.id;
+    }
+    public String getUser(){
+        return this.user;
+    }
+}
