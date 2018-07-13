3bf2519a3a152f30b2a4e04d85845f125e9d65b2
Sasanka Abeysinghe <sasanka.abeysinghe@ttu.edu>
2018-07-13 10:11:53 -0500

    twitter counts addition

--- src/java/com/oracle/WSClient.java
+++ src/java/com/oracle/WSClient.java
@@ -7,79 +7,31 @@ package com.oracle;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- *
- * @author sasanka
- */
+
 public class WSClient {
-    
-    public  static String testJSP(String str) {
+
+    public static String testJSP(String str) {
         return str.toLowerCase();
     }
-
+    
     public static Tweets requestItem() {
         Client client = ClientBuilder.newClient();
-
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
         Tweets twt = new Tweets();
-        
-        String filterOutput = "";
-        System.out.println("TRY");
         try {
-             twt = objectMapper.readValue(new URL("http://localhost:8080/statictweets"), Tweets.class);
-
-            filterOutput += "<table>";
-            int count = twt.tweets.size();
-            int stepCount = 0;
-
-            for (int i = 0; i < twt.tweets.size(); i++) {
-                if (twt.tweets.get(i).id != null) {
-                    filterOutput += "<tr><td>";
-                    filterOutput += twt.tweets.get(i).user.name + "</td>";
-                    String IdToString = twt.tweets.get(i).user.name;
-                    filterOutput += "</tr>";
-                    filterOutput += "<tr><td>";
-                    filterOutput += "@" + twt.tweets.get(i).user.screen_name + "</td>";
-                    filterOutput += "</tr>";
-                    filterOutput += "<tr><td>";
-                    filterOutput += twt.tweets.get(i).text + "</td>";
-                    filterOutput += "</tr>";
-                    filterOutput += "<tr><td>";
-                    filterOutput += "==========================================" + "</td>";
-                    filterOutput += "</tr>";
-                }
-            }
-            int f = stepCount;
-            filterOutput += "</table>";
+            twt = objectMapper.readValue(new URL("http://localhost:8080/statictweets"), Tweets.class);
 
         } catch (IOException ex) {
             Logger.getLogger(WSClient.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         return twt;
-    }
-
-    private static void requestOrderById(Client client, String id) {
-        WebTarget target = client.target("http://localhost:8080/statictweets" + id);
-        String s = target.request().get(String.class);
-        System.out.println("response : " + s);
     }
 }
